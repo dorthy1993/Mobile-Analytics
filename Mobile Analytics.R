383d1eef7fcfc0d3c4899aeecda27e9d0e3c803b
dorthy1993 <35855193+dorthy1993@users.noreply.github.com>
2019-03-15 21:23:53 -0700

    Update Mobile Analytics.R

--- Mobile Analytics.R
+++ Mobile Analytics.R
@@ -1,11 +1,16 @@
+Geo_fence<-read.csv(file.choose())
 
 #Create variable: "imp_large", "cat_entertainment", "cat_social", "cat_tech", "os_ios", "distance"
+
+attach(Geo_fence)
 
 Geo_fence$imp_large <- ifelse(imp_size == "728x90", 1, 0)
 Geo_fence$cat_entertainment <-ifelse(app_topcat == "IAB1" | app_topcat == "IAB1-6", 1, 0)
 Geo_fence$cat_social <- ifelse(app_topcat == "IAB14", 1, 0)
 Geo_fence$cat_tech <- ifelse(app_topcat == "IAB19-6", 1, 0)
 Geo_fence$os_ios <- ifelse(device_os == "iOS", 1, 0)
+
+library(aspace)
 Geo_fence$distance <- 6371 * acos(cos(as_radians(device_lat)) * cos(as_radians(geofence_lat)) 
                                  * cos(as_radians(device_lon) - as_radians(geofence_lon)) 
                                  + sin(as_radians(device_lat)) * sin(as_radians(geofence_lat)))
@@ -18,6 +23,8 @@ Geo_fence$distance_group <-ifelse(between(distance, 0, 0.5), 1,
                                                        ifelse(between(distance, 4, 7), 5,
                                                               ifelse(between(distance, 7, 10), 6, 
                                                                      ifelse(distance >10, 7, NA)))))))
+
+attach(Geo_fence)
 Geo_fence <- Geo_fence%>%group_by(distance_group)%>%
   mutate(click_through_rate = mean(didclick))
 
@@ -26,6 +33,8 @@ Geo_fence$ln_app_review_vol <- log(app_review_vol)
 Geo_fence$distance_squared <- distance^2
 
 #Correlations between the variables
+library(corrgram)
+
 cor_geofence <- subset(Geo_fence, select = c(didclick, distance, imp_large, cat_entertainment, cat_social, cat_tech, os_ios, ln_app_review_vol, app_review_val))
 cor(cor_geofence)
 corrgram(cor_geofence, order = TRUE, lower.panel = panel.shade,
@@ -33,6 +42,9 @@ corrgram(cor_geofence, order = TRUE, lower.panel = panel.shade,
          main = "Correlations of Geo-fense")
 
 #Scatterplot of distance and click-through-rate
+library(car)
+attach(Geo_fence)
+
 scatterplot(click_through_rate ~ distance, xlab = "distance", 
             ylab = "click_through_rate", main = "distance and click-through-rate")
 
